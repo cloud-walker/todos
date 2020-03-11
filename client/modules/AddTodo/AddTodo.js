a735471c5715019557c3df758deb122e0dea36f0
Luca Barone <baro.luc@gmail.com>
2020-03-11 17:39:54 +0100

    Involute the project for the sake of Stefano's learning

--- client/modules/AddTodo/AddTodo.js
+++ client/modules/AddTodo/AddTodo.js
@@ -1,27 +1,3 @@
-import React from 'react'
-
-import {useTodoMutation} from '../todos'
-
 export const AddTodo = () => {
-  const [mutate] = useTodoMutation()
-  const [value, setValue] = React.useState('')
-
-  const handleChange = e => {
-    setValue(e.target.value)
-  }
-  const handleSubmit = e => {
-    e.preventDefault()
-    mutate({title: value})
-    setValue('')
-  }
-
-  return (
-    <form onSubmit={handleSubmit} noValidate>
-      <label>
-        <span>Todo</span>
-        <input type="text" value={value} onChange={handleChange} />
-      </label>
-      <input type="submit" value="Submit" />
-    </form>
-  )
+  return null
 }
--- client/modules/TodosList/TodosList.js
+++ client/modules/TodosList/TodosList.js
@@ -1,27 +1,3 @@
-import React from 'react'
-
-import {useTodosQuery} from '../todos'
-
 export const TodosList = () => {
-  const {status, error, data} = useTodosQuery()
-
-  if (status == 'loading') {
-    return <h2>Loading...</h2>
-  }
-
-  if (status == 'error') {
-    return <h2>Error: {error.message}</h2>
-  }
-
-  if (!data.length) {
-    return <div>Woah! Niente da fare!</div>
-  }
-
-  return (
-    <ul>
-      {data.map(todo => (
-        <li key={todo.id}>{todo.title}</li>
-      ))}
-    </ul>
-  )
+  return null
 }
